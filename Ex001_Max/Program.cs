789f21377c7bc1bdd652e413e0bc0ede7431752e
alekseichernyshev <adanayrel@mail.ru>
2022-12-01 22:54:01 +0900

    Вторая задача чётное не чётное

--- Ex001_Max/Program.cs
+++ Ex001_Max/Program.cs
@@ -1,6 +1,6 @@
 ﻿// start
 
-int a = 2, b = 3, c = 7;
+int a = 44, b = 5, c = 78;
 int max = a;
 
 if (b > max ){
---
+++ Ex002_Even/Program.cs
@@ -0,0 +1,15 @@
+﻿
+// start
+
+int number = 7;
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число чётное");
+}
+else
+{
+    Console.WriteLine("Число не чётное");
+}
+
+// end
