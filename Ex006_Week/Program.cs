5369fed1d179a01119609cfffe34de90afca4229
alekseichernyshev <adanayrel@mail.ru>
2022-12-05 19:14:57 +0900

    сделали шестую задачу

---
+++ Ex006_Week/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+Console.WriteLine("Пожалуйста, введите цифру от 1 до 7 и нажмите клавишу Enter\t");
+int number = int.Parse(Console.ReadLine());
+
+if (number > 0 && number < 8)
+{
+    if (number > 5)
+    {
+        Console.WriteLine("Выходной день");
+    }
+    else
+    {
+        Console.WriteLine("Будний день");
+    }
+}
+else
+{
+    Console.WriteLine("Увы, такого дня недели не существует");
+}
