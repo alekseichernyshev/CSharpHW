faf3b8209b5f37b2c20cfdae4cf0ac32cf381189
alekseichernyshev <adanayrel@mail.ru>
2022-12-05 23:36:53 +0900

    Сделали седьмую задачу

---
+++ Ex007_Pal/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Пожалуйста, введите пяти значное число и нажмите клавишу Enter\t");
+int number = int.Parse(Console.ReadLine());
+
+if(number > 999 && number < 100000)
+{
+    int numA = number / 10000 % 10;
+    int numB = number / 1000 % 10;
+    int numC = number / 10 % 10;
+    int numD = number % 10;
+    Console.WriteLine(numD);
+    if(numA == numD && numB == numC)
+    {
+        Console.WriteLine("Данное число является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Данное число не является палиндромом");
+    }
+}
+else
+{
+    Console.WriteLine("Error");
+}
