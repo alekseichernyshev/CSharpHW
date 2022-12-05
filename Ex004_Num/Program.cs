99c0a1ec56d6b18107f9f8cece5f34848ebd5d19
alekseichernyshev <adanayrel@mail.ru>
2022-12-05 19:38:19 +0900

    Исправили четвёртую задачу

---
+++ Ex004_Num/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Пожалуйста, введите трёхзначное число и нажмите клавишу Enter\t");
+int number = int.Parse(Console.ReadLine());
+if (number > 99 && number < 1000)
+{
+    Console.WriteLine((number / 10) % 10);
+}
+else
+{
+    Console.WriteLine("Error");
+}
