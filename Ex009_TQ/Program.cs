dce164e10c7e8d05d5780891baad604964f85203
alekseichernyshev <adanayrel@mail.ru>
2022-12-07 22:48:45 +0900

    Сделали девятую задачу

---
+++ Ex009_TQ/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Пожалуйста, введите число и нажмите клавишу Enter\t");
+int number = int.Parse(Console.ReadLine());
+Console.Write("Таблица кубов для числа\t");
+Console.WriteLine(number);
+
+for (int i = 1; i < number + 1; i++)
+{
+    Console.WriteLine(i * i * i);
+}
