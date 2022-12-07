83f0861e8681bc641f4f2fa68f0400b9ce852760
alekseichernyshev <adanayrel@mail.ru>
2022-12-07 22:21:24 +0900

    Восьмая задача завершена

---
+++ Ex008_Coor/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+Console.WriteLine("Пожалуйста, введите первое значение первой точки Enter\t");
+Double xa = Double.Parse(Console.ReadLine());
+Console.WriteLine("Пожалуйста, введите второе значение первой точки Enter\t");
+Double ya = Double.Parse(Console.ReadLine());
+Console.WriteLine("Пожалуйста, введите первое значение второй точки Enter\t");
+Double xb = Double.Parse(Console.ReadLine());
+Console.WriteLine("Пожалуйста, введите второе значение второй точки Enter\t");
+Double yb = Double.Parse(Console.ReadLine());
+Console.WriteLine(Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2)));
