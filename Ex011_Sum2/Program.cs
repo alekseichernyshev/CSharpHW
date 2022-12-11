d70630f04da7d266849572c3d61ad7ce5130d019
alekseichernyshev <adanayrel@mail.ru>
2022-12-11 19:50:32 +0900

    Одиннадцатая  задача

---
+++ Ex011_Sum2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. Строки использовать нельззя
+
+Console.WriteLine("Введите число\t");
+int number = int.Parse(Console.ReadLine());
+int sum = 0;
+//if (number < 0)
+//{
+//    number *= -1;   // делаем число положительным
+//}
+
+while (number != 0)
+{
+    sum += number % 10;
+    number /= 10;
+}
+
+Console.WriteLine(sum);
