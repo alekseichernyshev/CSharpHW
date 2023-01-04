d7355c873adb5c4e8e0838936546d521c794d99b
alekseichernyshev <adanayrel@mail.ru>
2023-01-04 14:24:52 +0900

    задача 66

---
+++ Ex025/Program.cs
@@ -0,0 +1,11 @@
+﻿// метод суммирования элементов
+static int SumElements(int m, int n)
+{
+    int result = m;
+    for (int i = m+1; i <= n; i++)
+    {
+        result += i;
+    }
+    return result;
+}
+Console.WriteLine(SumElements(10, 14));
