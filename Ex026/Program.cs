33dc6d81cb090b1794b174f6b036ff0ec5a54de1
alekseichernyshev <adanayrel@mail.ru>
2023-01-04 14:45:08 +0900

    задача 68

---
+++ Ex026/Program.cs
@@ -0,0 +1,19 @@
+﻿// метод решения
+static int Accerman(int n, int m)
+{
+    if (n == 0)
+
+        return m + 1;
+
+    else
+
+        if ((n != 0) && (m == 0))
+        return Accerman(n - 1, 1);
+    else
+
+        return Accerman(n-1, Accerman(n, m-1));
+
+}
+
+
+Console.WriteLine(Accerman(2, 3));
