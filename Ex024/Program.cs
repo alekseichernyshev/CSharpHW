4e235ed3bb7ce5b58c12ac4ff4582af074ec09dc
alekseichernyshev <adanayrel@mail.ru>
2023-01-04 14:10:16 +0900

    задача 64

---
+++ Ex024/Program.cs
@@ -0,0 +1,15 @@
+﻿
+// метод печати
+void PrintNumbers(int n)
+{
+    for (int i = n; i > 0; i--)
+    {
+        Console.Write($"{n} ");
+        n--;
+    }
+    return;
+
+
+}
+
+PrintNumbers(8);
